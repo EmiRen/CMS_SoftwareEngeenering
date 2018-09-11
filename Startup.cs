46cc91e61f29e7c4a9cfda364c6e9332101afb3a
J.Ren <42492651+EmiRen@users.noreply.github.com>
2018-09-11 23:35:51 +1000

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,80 @@
+﻿using CMS4.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMS4.Startup))]
+namespace CMS4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "Admin";
+                user.Email = "Admin1@gmail.com";
+
+                string userPWD = "Admin1@gmail.com";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+
+            if (!roleManager.RoleExists("Chair"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Chair";
+                roleManager.Create(role);
+
+            }
+
+
+            if (!roleManager.RoleExists("Reviewer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Reviewer";
+                roleManager.Create(role);
+
+            }
+
+            if (!roleManager.RoleExists("Author"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Author";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+}
